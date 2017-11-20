5e62c51b320084cf9125b641fd8a0ab01694b18d
DeckerCHAN <DeckerCHAN@gmail.com>
2017-11-21 03:14:05 +1100

    Initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  active = false;
+
+  toggleMenuOn(event) {
+    event.preventDefault();
+    this.active = !this.active;
+  }
+
+  toggleMenuOff(event) {
+    event.preventDefault();
+    this.active = !this.active;
+
+  }
 }
